aacd96a55acd80d769dece6efc682dfc50d74b94
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2023-08-27 13:54:02 -0400

    First implementation of python models.

--- politystock_backend/politystock_api/models.py
+++ politystock_backend/politystock_api/models.py
@@ -1,3 +1,29 @@
 from django.db import models
 
 # Create your models here.
+
+
+class Politician(models.Model):
+    name = models.CharField(max_length=255)
+    party = models.CharField(max_length=255)
+    state = models.CharField(max_length=255)
+    district = models.CharField(max_length=255)
+
+
+class Stock(models.Model):
+    symbol = models.CharField(max_length=255)
+    company = models.CharField(max_length=255)
+
+
+class Transaction(models.Model):
+    politician = models.ForeignKey(Politician, on_delete=models.CASCADE)
+    stock = models.ForeignKey(Stock, on_delete=models.CASCADE)
+    transaction_date = models.DateField()
+    TRANSACTION_TYPES = [
+        ('Buy', 'Buy'),
+        ('Sell', 'Sell'),
+        ('Dividend', 'Dividend'),
+    ]
+    transaction_type = models.CharField(
+        max_length=255, choices=TRANSACTION_TYPES)
+    amount = models.DecimalField(max_digits=10, decimal_places=2)
