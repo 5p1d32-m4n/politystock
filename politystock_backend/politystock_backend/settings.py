eb66b7c89b9eb30c8bce6343dc228eced5be452e
Brandon J. Baker <brandon.j.baker1011@gmail.com>
2023-08-27 13:54:22 -0400

    added database settings.

--- politystock_backend/politystock_backend/settings.py
+++ politystock_backend/politystock_backend/settings.py
@@ -74,9 +74,18 @@ WSGI_APPLICATION = 'politystock_backend.wsgi.application'
 # https://docs.djangoproject.com/en/4.2/ref/settings/#databases
 
 DATABASES = {
+    # 'default': {
+    #     'ENGINE': 'django.db.backends.sqlite3',
+    #     'NAME': BASE_DIR / 'db.sqlite3',
+    # }
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'politystock',
+        # TODO: These need to be in a .env file for it to be in system variable instead of a local one.
+        'USER': 'djangoAdmin',
+        'PASSWORD': 'think harder u-1',
+        'HOST': 'localhost',
+        'PORT': '3306',
     }
 }
 
